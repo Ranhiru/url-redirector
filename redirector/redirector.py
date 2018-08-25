1273fa504852b49d4fac040b7a81e91959434add
Ranhiru Cooray <ranhiru@gmail.com>
2018-08-25 17:00:45 +0800

    Added tests for get_unique_token

--- redirector/redirector.py
+++ redirector/redirector.py
@@ -1,5 +1,10 @@
+import secrets
+
 def create_redirection(event, context):
     return {
       "statusCode": 200,
       "body": { "key": "XXXX-YYYY-ZZZZZ" }
     }
+
+def get_unique_token(token_size):
+  return secrets.token_urlsafe(token_size)
--- tests/redirector/test_redirector.py
+++ tests/redirector/test_redirector.py
@@ -1,5 +1,14 @@
+import secrets
+
 from redirector.redirector import create_redirection
+from redirector.redirector import get_unique_token
 
 def test_url_redirection():
     result = create_redirection({}, {})
     assert result["statusCode"] == 200
+
+def test_generate_unique_key(mocker):
+    mocker.patch.object(secrets, 'token_urlsafe')
+    secrets.token_urlsafe.return_value = "XXXX"
+    assert get_unique_token(10) == "XXXX"
+    secrets.token_urlsafe.assert_called_once_with(10)
