e75e6aa3e8d07db5fda0a6812838b2c424303660
Ranhiru Cooray <ranhiru@gmail.com>
2018-09-01 15:41:20 +0800

    Added code for save_url and calling it from the create_redirection method

--- redirector/redirector.py
+++ redirector/redirector.py
@@ -1,10 +1,33 @@
+import json
 import secrets
+import boto3
+import os
+from datetime import datetime
 
 def create_redirection(event, context):
+    body = json.loads(event["body"])
+    url = body["url"]
+
+    key = save_url(url)
+    response = {'key': key}
+
     return {
-      "statusCode": 200,
-      "body": { "key": "XXXX-YYYY-ZZZZZ" }
+        "statusCode": 200,
+        "body": json.dumps(response)
     }
+
+def save_url(url):
+    key = get_unique_token(os.getenv("TOKEN_SIZE", 10))
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.getenv('TABLE_NAME'))
+    table.put_item(
+       Item={
+            'Key': key,
+            'url': url,
+            'created_at_utc': str(datetime.utcnow())
+        }
+    )
+    return key
 
 def get_unique_token(token_size):
   return secrets.token_urlsafe(token_size)
